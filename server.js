c457b371de9ee0b1788cb681842673fca9a76c46
freddykaberuka <freddykaberuka2009@gmail.com>
2022-03-02 23:04:42 +0200

    setting server

---
+++ server.js
@@ -0,0 +1,4 @@
+import app from './src/index'
+const port = 4000;
+
+app.listen(port, console.log(`listening to a port .... ${port}`))
---
+++ src/database/migrations/20220302194107-create-products.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      item: {
+        type: Sequelize.STRING
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
